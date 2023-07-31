8108ac7edfe3a73486823c43fb43b42399b66f3b
kameshwar mehta <2101020695@cgu-odisha.ac.in>
2023-07-31 22:05:13 +0530

    initial commit

---
+++ com.mehta.scaler/src/main/Application/Application.java
@@ -0,0 +1,9 @@
+package main.Application;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+	}
+
+}
---
+++ com.mehta.scaler/src/main/biswadip/Sort012.java
@@ -0,0 +1,10 @@
+package main.biswadip;
+
+public class Sort012 {
+	
+	public int[] sort(int[] arr) {
+		//implement
+		
+		return null;
+	}
+}
