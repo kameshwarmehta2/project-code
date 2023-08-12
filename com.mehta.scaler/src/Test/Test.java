829b2b8ee6b3ce549bf501f4104335668207606c
kameshwar mehta <2101020695@cgu-odisha.ac.in>
2023-08-12 22:04:54 +0530

    updated with code

--- com.mehta.scaler/src/Test/Test.java
+++ com.mehta.scaler/src/Test/Test.java
@@ -1,5 +1,33 @@
 package Test;
+import java.util.Arrays;
 
 public class Test {
+    public int solve(int[] A) {
+     Arrays.sort(A);
+     int max=A[A.length-1];
+     int min=A[0];
+     int count=0;
+     int sec_max=0;
+     int sec_min=0;
+      
+     for(int i=1;i<A.length;i++){
+          if(A[i]>min){
+              sec_min=i;
+              break;
+          }
+      }
+     for(int i=A.length-2;i>=0;i--){
+          if(A[i]<max){
+              sec_max=i;
+              break;
+          }
+     }
+     
+    if(sec_max!=0)
+        count=(sec_max-sec_min)+1;
 
+     return count;
+     
+    }
 }
+
